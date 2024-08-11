c7e31bdc1a8df894f181fa864efbdf8ae372a27e
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-11 12:58:13 +0530

    Exception handler

--- src/main/java/com/example/DataIntegrityException/Handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/DataIntegrityException/Handler/GlobalExceptionHandler.java
@@ -12,12 +12,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleDataIntegrityException(DataIntegrityViolationException exception){
         String errorMessage="DataBase Error Occurred!";
-        if (exception.getMessage().contains("username")){
+        if (exception.getMessage().contains("(user_name)")){
             errorMessage="Username Already Taken!";
-        } else if (exception.getMessage().contains("phone_no")) {
-            errorMessage="Phone No Alredy Exist";
+        } else if (exception.getMessage().contains("(phone_no)")) {
+            errorMessage="Phone No Already Exist";
         }
-        else if (exception.getMessage().contains("account_no")){
+        else if (exception.getMessage().contains("(account_no)")){
             errorMessage="account no already exist";
         }
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
