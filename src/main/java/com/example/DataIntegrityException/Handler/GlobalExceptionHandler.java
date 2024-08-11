ebb9b57bf466f06f74f41c2eb2fdafc7f9d729e3
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-11 12:03:10 +0530

    Exception Handler

---
+++ src/main/java/com/example/DataIntegrityException/Handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.DataIntegrityException.Handler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityException(DataIntegrityViolationException exception){
+        String errorMessage="DataBase Error Occurred!";
+        if (exception.getMessage().contains("username")){
+            errorMessage="Username Already Taken!";
+        } else if (exception.getMessage().contains("phone_no")) {
+            errorMessage="Phone No Alredy Exist";
+        }
+        else if (exception.getMessage().contains("account_no")){
+            errorMessage="account no already exist";
+        }
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
