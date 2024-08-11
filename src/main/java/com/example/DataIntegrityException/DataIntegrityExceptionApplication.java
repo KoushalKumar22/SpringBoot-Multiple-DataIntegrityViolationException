d85f1a755f24bb22c778684e5fde8214bb018a6a
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-11 11:29:45 +0530

    Deployment

---
+++ src/main/java/com/example/DataIntegrityException/DataIntegrityExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataIntegrityException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIntegrityExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIntegrityExceptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DataIntegrityException/DataIntegrityExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataIntegrityException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIntegrityExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
