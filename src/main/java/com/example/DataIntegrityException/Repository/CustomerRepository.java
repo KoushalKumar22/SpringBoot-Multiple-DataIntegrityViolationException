b39dc3557dff9fcdb31a654bc15c9cbf1055ae1a
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-11 12:57:54 +0530

    Repository

--- src/main/java/com/example/DataIntegrityException/Repository/CustomerRepository.java
+++ src/main/java/com/example/DataIntegrityException/Repository/CustomerRepository.java
@@ -1,9 +1,10 @@
 package com.example.DataIntegrityException.Repository;
 
+import com.example.DataIntegrityException.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.lang.reflect.Member;
 @Repository
-public interface MemberRepository extends JpaRepository<Member,Integer> {
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
